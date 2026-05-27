d5986a777d30e9ec9ace195b5821719a8c60ff90
Iker Pobo Ordonez <122252776+Adahii@users.noreply.github.com>
2026-05-27 17:58:16 -0400

    Create en-us.lua for Gambler Joker

---
+++ Jokers/Gambler/en-us.lua
@@ -0,0 +1,9 @@
+			j_mp_gambler = {
+				name = "Gambler",
+				text = {
+					"After defeating the {C:attention}Boss Blind{},",
+					"roll for {C:mult}+0{} to {C:mult}+4{} to this",
+					"Joker's {X:mult,C:white}X{}Mult (stacks permanently)",
+					"{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive})",
+				},
+			},
